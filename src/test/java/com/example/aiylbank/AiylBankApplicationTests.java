f2aa9d0e3f1662fac2260b21246e403f3e7e864a
Dautov Muslim <dautov.292009@gmail.com>
2026-03-03 19:24:35 +0600

    First commit

---
+++ src/test/java/com/example/aiylbank/AiylBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiylbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiylBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
